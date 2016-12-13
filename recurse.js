4bdd552ac16761b87f234b1fb48da2a93130173d
Pierre Gambarotto <pierre.gambarotto@enseeiht.fr>
2016-12-13 11:47:48 +0100

    sum & test

--- recurse.js
+++ recurse.js
@@ -1,1 +1,8 @@
-module.exports = true
+const sum = (a) => {
+  if (a.length === 0)
+    return 0
+  else
+    return a[0] + sum(a.slice(1))
+}
+
+module.exports = {sum: sum}
--- test/recurse-test.js
+++ test/recurse-test.js
@@ -1,10 +1,22 @@
 // recurse-test
-const rec = require('../recurse')
-
 const expect = require('chai').expect
+const sum = require('../recurse').sum
 
-describe('working config', () => {
-  it('has a sample test', () => {
-    expect(rec).to.be.true
+
+
+
+describe('sum', () => {
+  it('returns 0 for an empty array', () => {
+    expect(sum([])).to.equal(0)
+  })
+  it('returns 456 for [2,3,456,-2,-3]', () => {
+    const a = [2,3,456,-2,-3]
+    expect(sum(a)).to.equal(456)
+
+  })
+  it('returns 45 for [2,3,45,-2,-3]', () => {
+    const a = [2,3,45,-2,-3]
+    expect(sum(a)).to.equal(45)
+
   })
 })
