7fd0381b8750b1e6bd473c2a1e2fb82450a4610a
Pierre Gambarotto <pierre.gambarotto@enseeiht.fr>
2016-12-13 11:13:11 +0100

    initial version

---
+++ test/recurse-test.js
@@ -0,0 +1,10 @@
+// recurse-test
+const rec = require('../recurse')
+
+const expect = require('chai').expect
+
+describe('working config', () => {
+  it('has a sample test', () => {
+    expect(rec).to.be.true
+  })
+})
